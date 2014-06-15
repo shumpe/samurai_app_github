2a7bf6d4b5744a16ddda23cf4ff84d7f3ff2756b
齊藤 旬平 <kuritaro0126@gmail.com>
2014-06-15 17:00:35 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamuraiApp::Application.config.secret_token = 'fd52f01d173e1ed09133afed8dfe6e1fc382869996d951159e64ecb5f1b939e31bdca77b8cf6ae4f4b90f3e3f00894823937009352751259abb119daab804cfe'
